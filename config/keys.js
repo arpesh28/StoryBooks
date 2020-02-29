da0411b15288318b94a011a960edde9e1d895f9a
arpesh28 <arpeshgadekar@gmail.com>
2020-02-29 09:36:58 +0530

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://desmond:desmond28@ds029814.mlab.com:29814/storybooks-app',
+    googleClientID:'738384208292-aj57acv31fl8eakh5lkrksgpo04aqvpe.apps.googleusercontent.com',
+    googleClientSecret:'LMtWo7Ns1jqOcBbMRGgnmP_X'
+}
