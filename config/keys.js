688aeeb2af4a6ae99d51e12f13ad6d7b2b0f95fe
Arpesh Gadekar <44309224+arpesh28@users.noreply.github.com>
2020-07-10 10:58:51 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://desmond:desmond28@ds029814.mlab.com:29814/storybooks-app',
-    googleClientID:'738384208292-aj57acv31fl8eakh5lkrksgpo04aqvpe.apps.googleusercontent.com',
-    googleClientSecret:'LMtWo7Ns1jqOcBbMRGgnmP_X'
+    mongoURI: /*mongo db url here*/,
+    googleClientID:/*Google Client Id here*/,
+    googleClientSecret:/*Google Client secret here*/
 }
